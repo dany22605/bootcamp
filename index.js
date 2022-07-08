ffc73c050a2d1daa073ab7f69def1a2bd7b41b09
dany22605 <108540009+dany22605@users.noreply.github.com>
2022-07-08 17:07:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+
+// Ejercicio 2
+
+const datosPersonales = {
+    nombre: "Daniel Ferrer Gomez",
+    edad: 33,
+    desarrollador: false,
+    fecha: new Date ( "may 1989 22"),
+    libro:{
+        titulo: "one piece",
+        auto: "eichiro oda",
+        fecha: new Date ( "july 1997 22"),
+        url: "https://es.wikipedia.org/wiki/One_Piece",
+
+    }
+
+}
+console.log(datosPersonales);
